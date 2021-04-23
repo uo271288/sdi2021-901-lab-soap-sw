a039011ae94d47f40d3140bbfe9c33d275e501db
uo271288 <uo271288@uniovi.es>
2021-04-23 21:31:22 +0200

    SDI-2021-901-SW-11.1-Servicio SOAP

---
+++ sdi2021-901-lab-soap-sw/src/main/java/com/uniovi/Sdi2021901LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021901LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021901LabSoapSwApplication.class, args);
+	}
+
+}
---
+++ sdi2021-901-lab-soap-sw/src/main/java/com/uniovi/soap/ws/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.04.23 a las 08:58:54 PM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/soap/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uniovi.soap.ws;
