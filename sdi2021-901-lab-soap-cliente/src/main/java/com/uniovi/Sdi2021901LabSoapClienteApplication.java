f5a564077b397a39a0cf180ed7e6bddcd0ab0866
uo271288 <uo271288@uniovi.es>
2021-04-23 22:04:26 +0200

    SDI-2021-901-SW-11.2- Servicio SOAP Cliente

---
+++ sdi2021-901-lab-soap-cliente/src/main/java/com/uniovi/Sdi2021901LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021901LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021901LabSoapClienteApplication.class, args);
+	}
+
+}
